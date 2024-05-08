a8c3b2833fc9fa2ed7594411112793f50b97b514
Clément <clement2104.boillot@gmail.com>
2024-05-08 16:52:13 +0200

    feat: improve performances
    Co-authored-by: Sigmanificient <edhyjox@gmail.com>

--- gameoflife.c
+++ gameoflife.c
@@ -5,17 +5,19 @@
 #include <string.h>
 #include <time.h>
 
+#define WIDTH ((2 << 9) - 1)
+#define HEIGHT ((2 << 9) - 1)
+
 typedef enum
 {
-    Dead = 0,
-    Alive = 1,
+    Dead = false,
+    Alive = true,
 } tile_t;
 
 typedef struct
 {
-    const int width;
-    const int height;
-    tile_t *data;
+    bool *data;
+    bool *last;
     struct
     {
         int x;
@@ -23,61 +25,41 @@ typedef struct
     } camera;
 } board_t;
 
-static tile_t get_tile(const board_t *b, int x, int y)
-{
-    if (x < 0 || x >= b->width || y < 0 || y >= b->height)
-        return Dead;
-    return b->data[y * b->width + x];
-}
+#define IDX(x, y) (((y) & HEIGHT) * WIDTH + ((x) & WIDTH))
 
-static void set_tile(board_t *b, int x, int y, tile_t value)
-{
-    if (x < 0 || x >= b->width || y < 0 || y >= b->height)
-        return;
-    b->data[y * b->width + x] = value;
-}
+#define foreach_cell(x, y) \
+    for (int (y) = 0; (y) < HEIGHT; ++(y)) \
+        for (int (x) = 0; (x) < WIDTH; ++(x))
 
-static size_t count_neighbors(const board_t *b, int x, int y)
+static size_t count_neighbors(const bool *board, int x, int y)
 {
-    size_t count = 0;
-
-    for (int dx = -1; dx <= 1; ++dx) {
-        for (int dy = -1; dy <= 1; ++dy) {
-            if (dx == 0 && dy == 0)
-                continue;
-            if (get_tile(b, x + dx, y + dy) == Alive)
-                count++;
-        }
-    }
-    return count;
+    return (
+        board[IDX(x - 1, y - 1)]
+        + board[IDX(x, y - 1)]
+        + board[IDX(x + 1, y - 1)]
+        + board[IDX(x - 1, y)]
+        + board[IDX(x + 1, y)]
+        + board[IDX(x - 1, y + 1)]
+        + board[IDX(x, y + 1)]
+        + board[IDX(x + 1, y + 1)]
+    );
 }
 
 static void update_board(board_t *b)
 {
-    board_t copy = {
-        .width = b->width,
-        .height = b->height,
-        .data = malloc(((size_t)b->width * b->height) * sizeof *b->data),
-    };
-    if (copy.data == NULL) {
-        perror("malloc");
-        return;
-    }
-    memcpy(copy.data, b->data, (size_t)b->width * b->height * sizeof *b->data);
+    bool *tmp = b->data;
+    b->data = b->last;
+    b->last = tmp;
 
-    for (int x = 0; x < b->width; ++x) {
-        for (int y = 0; y < b->height; ++y) {
-            size_t neighbors = count_neighbors(&copy, x, y);
-            if (get_tile(&copy, x, y) == Alive) {
-                if (neighbors < 2 || neighbors > 3)
-                    set_tile(b, x, y, Dead);
-            } else {
-                if (neighbors == 3)
-                    set_tile(b, x, y, Alive);
-            }
-        }
+    foreach_cell(x, y) {
+        size_t neighbors = count_neighbors(b->last, x, y);
+        if (neighbors == 3)
+            b->data[IDX(x, y)] = Alive;
+        else if (neighbors != 2)
+            b->data[IDX(x, y)] = Dead;
+        else
+            b->data[IDX(x, y)] = b->last[IDX(x, y)];
     }
-    free(copy.data);
 }
 
 static void draw_board(const board_t *b, uint8_t zoom)
@@ -85,13 +67,12 @@ static void draw_board(const board_t *b, uint8_t zoom)
     BeginDrawing();
     ClearBackground(BLACK);
 
-    for (int x = 0; x < b->width; ++x)
-        for (int y = 0; y < b->height; ++y)
-            if (get_tile(b, x, y) == Alive)
-                DrawRectangle(
-                    (x - b->camera.x) * zoom, (y - b->camera.y) * zoom, zoom,
-                    zoom, WHITE);
-
+    foreach_cell(x, y)
+        if (b->data[IDX(x, y)] == Alive)
+            DrawRectangle(
+                (x - b->camera.x) * zoom,
+                (y - b->camera.y) * zoom,
+                zoom, zoom, WHITE);
     EndDrawing();
 }
 
@@ -100,22 +81,19 @@ int main(void)
     srand(time(NULL));
     uint8_t zoom_level = 1;
 
-    board_t b = {
-        .width = 800,
-        .height = 450,
-        .data = NULL,
-    };
+    board_t b = {0};
 
-    b.data = malloc(((size_t)b.width * b.height) * sizeof *b.data);
+    // double malloc to have buffers side by side
+    b.data = malloc(2UL * WIDTH * HEIGHT * sizeof *b.data);
     if (b.data == NULL)
         return perror("malloc"), 1;
+    b.last = &b.data[1UL * WIDTH * HEIGHT];
 
     // set game at a random state
-    for (int x = 0; x < b.width; ++x)
-        for (int y = 0; y < b.height; ++y)
-            set_tile(&b, x, y, (rand() % 10 == 0) ? Alive : Dead);
+    foreach_cell(x, y)
+        b.data[IDX(x, y)] = (rand() % 10 == 0) ? Alive : Dead;
 
-    InitWindow(b.width, b.height, "Game of life");
+    InitWindow(WIDTH, HEIGHT, "Game of life");
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
